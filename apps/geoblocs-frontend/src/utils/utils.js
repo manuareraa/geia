89fe2f7924fbc5168266c6fe06e1c0d2b06e5fcf
Michael Browne <brownemt@gmail.com>
2024-01-25 21:08:55 +0000

    Handle if text is undefined

--- apps/geoblocs-frontend/src/utils/utils.js
+++ apps/geoblocs-frontend/src/utils/utils.js
@@ -1,4 +1,8 @@
 const textTrimmer = (text, length) => {
+    if ( text == undefined ) {
+        return text;
+    }
+
   if (text.length > length) {
     return text.substring(0, length) + "...";
   } else {
