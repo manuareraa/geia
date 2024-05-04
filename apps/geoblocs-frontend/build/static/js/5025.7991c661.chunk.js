c498d9ebf4663f8f5c6232ba191aacc6fae3b311
Manuareraa <manuareraa@gmail.com>
2024-05-04 11:29:18 +0530

    Build Created

---
+++ apps/geoblocs-frontend/build/static/js/5025.7991c661.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgeoblocs=self.webpackChunkgeoblocs||[]).push([[5025],{25025:function(e){e.exports=JSON.parse('[{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]')}}]);
---
+++ apps/geoblocs-frontend/build/static/js/6942.f7bba3e2.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkgeoblocs=self.webpackChunkgeoblocs||[]).push([[6942],{24654:function(){},52361:function(){},94616:function(){}}]);
